c4ba2d351ba147a047a3bdee571b6792f63808ea
Sharon <srcbun@gmail.com>
2020-06-18 15:59:47 -0400

    change any double quotes to single quotes to save my sanity

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import "./styles/styles.scss";
+import './styles/styles.scss';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
