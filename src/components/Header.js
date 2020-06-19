b1defdeb463a2cd48851ac5d597aa9242a0bc383
Sharon <srcbun@gmail.com>
2020-06-19 18:06:02 -0400

    add icon in header, change color of bg/header/icons

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCode } from '@fortawesome/free-solid-svg-icons'
+import { faCode, faCloudSun } from '@fortawesome/free-solid-svg-icons'
 
 const Header = () => (
   <header>
     <div className='nav'>
       <div className='nav-items'>
-        <h1 className='nav-title'>Can I Wear a Hoodie Today?</h1>
+        <h1 className='nav-title'><FontAwesomeIcon className='nav-title_icon' icon={faCloudSun} />Can I Wear a Hoodie Today?</h1>
         <a href='https://github.com/shaerins' target='_blank' rel='noopener noreferrer'><FontAwesomeIcon icon={faCode} /></a>
       </div>
     </div>
